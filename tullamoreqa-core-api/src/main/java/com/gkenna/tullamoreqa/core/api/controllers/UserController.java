3e28e82e6888296c3e7999ab6ff82b27ab571a27
Gavin Kenna <thegavinkenna@gmail.com>
2018-08-11 23:34:55 +0100

    Issue #14 Started work on Comment, Question, Tag and User Controller API checkstyle improvements.
    Signed-off-by: Gavin Kenna <thegavinkenna@gmail.com>

--- tullamoreqa-core-api/src/main/java/com/gkenna/tullamoreqa/core/api/controllers/UserController.java
+++ tullamoreqa-core-api/src/main/java/com/gkenna/tullamoreqa/core/api/controllers/UserController.java
@@ -7,38 +7,57 @@ package com.gkenna.tullamoreqa.core.api.controllers;
 import com.gkenna.tullamoreqa.domain.User;
 import org.springframework.http.ResponseEntity;
 
+/**
+ * API Controller for the {@link User} Entity. This API will allow
+ * external parties, i.e. UI or CLI, to Get/Add/Update/Delete Users.
+ * <p>
+ * The Implementation of this API will in turn call the
+ * {@link com.gkenna.tullamoreqa.core.api.services.UserService} API.
+ *
+ * @author Gavin Kenna
+ * @since 0.0.0
+ */
 public interface UserController {
     /**
-     * POST Method
+     * HTTP POST Method
+     * <p>
+     * Add an {@link User} to the Database.
      *
-     * @param input
-     * @return
+     * @param input An {@link User} container that should be inserted into
+     *              the Database.
+     * @return The Response of this Request.
      */
-    ResponseEntity<?> addUser(User input);
+    ResponseEntity<?> addUser(final User input);
 
     /**
-     * GET Method
+     * HTTP GET Method
+     * <p>
+     * Retrieve an {@link User} from the Database.
      *
-     * @param username
-     * @return
+     * @param username The ID of the {@link User} to retrieve.
+     * @return The Response of this Request.
      */
-    ResponseEntity<User> getUser(String username);
+    ResponseEntity<User> getUser(final String username);
 
     /**
-     * PUT Method
+     * HTTP PUT Method
+     * <p>
+     * Update an {@link User} on the Database.
      *
-     * @param username
-     * @param input
-     * @return
+     * @param username The ID of the {@link User} to update.
+     * @param input    An {@link User} container that holds
+     *                 new values for userId to update to.
+     * @return The Response of this Request.
      */
-    ResponseEntity<?> updateUser(String username, User input);
-
+    ResponseEntity<?> updateUser(final String username, final User input);
 
     /**
-     * DELETE Method
+     * HTTP DELETE Method.
+     * <p>
+     * Delete an {@link User} from the Database.
      *
-     * @param username
-     * @return
+     * @param username The ID of the {@link User} to delete.
+     * @return The Response of this Request.
      */
-    ResponseEntity<?> deleteUser(String username);
+    ResponseEntity<?> deleteUser(final String username);
 }
