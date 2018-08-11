b59d0c4d03d5523f9153c721dfedda14a6c272b1
Gavin Kenna <thegavinkenna@gmail.com>
2018-08-12 00:21:22 +0100

    Issue #14 Added package-info for API Repo.
    Signed-off-by: Gavin Kenna <thegavinkenna@gmail.com>

---
+++ tullamoreqa-core-api/src/main/java/com/gkenna/tullamoreqa/core/api/repositories/package-info.java
@@ -0,0 +1,9 @@
+/*
+ * Copyright (c) 2018. Gavin Kenna
+ */
+
+/**
+ * Location of all Repository APIs.
+ * Repo's communicate directly with the Database via Hibernate.
+ */
+package com.gkenna.tullamoreqa.core.api.repositories;
