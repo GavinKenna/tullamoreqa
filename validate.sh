dc97d94538aeecbabf2a5e56f0c620c1675ef781
Gavin <Gavin.Kenna@ittd.ie>
2018-08-15 13:27:35 +0100

    Added validate.sh script for Linux/Mac

---
+++ validate.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+#==============================================================================
+#
+#          FILE:  validate.sh
+#
+#         USAGE:  ./validate.sh
+#
+#   DESCRIPTION: Build TullamoreQA and all of it's components.
+#                Will run Unit Tests, Integration Tests, Javadocs and Checkstyle.
+#
+#       OPTIONS:  ---
+#  REQUIREMENTS:  ---
+#          BUGS:  ---
+#         NOTES:  ---
+#        AUTHOR:  Gavin Kenna <thegavinkenna@gmail.com>, gkenna
+#       COMPANY:  Gavin Kenna
+#       VERSION:  1.0
+#       CREATED:  15/08/2018
+#      REVISION:  ---
+#==============================================================================
+
+# Confirm Docker is installed.
+
+if type -p docker; then
+    _docker=docker
+else
+    echo "Docker is not installed. Docker is required for the
+    TullamoreQA Integration Tests."
+    exit 1
+fi
+
+# Run build.sh
+
+./build.sh
+
+# Execute build of TQA
+
+mvn checkstyle:check javadoc:javadoc -Ptest verify
