6176da408348cd711eb932ee2e0cdd60d002da5f
Gavin <thegavinkenna@gmail.com>
2018-11-12 20:55:42 +0000

    #24 Finalised Vote JavaDocs requirements.

--- tullamoreqa-domain/src/main/java/com/gkenna/tullamoreqa/domain/Vote.java
+++ tullamoreqa-domain/src/main/java/com/gkenna/tullamoreqa/domain/Vote.java
@@ -21,25 +21,33 @@ import java.time.Instant;
 import java.util.Date;
 import java.util.Objects;
 
+/**
+ * Vote Domain. Contains information about what {@link Entry} is being Voted on,
+ * what {@link User} cast the Vote and what
+ * {@link VoteType} it is (Upvote or Downvote).
+ *
+ * @author Gavin Kenna
+ * @since 0.0.11
+ */
 @Entity
 @Table(name = "votes")
 public class Vote implements Domain {
 
     /**
-     *
+     * ID of the Vote.
      */
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
     /**
-     *
+     * The {@link VoteType} of this Vote, i.e. be it Upvote or Downvote.
      */
     @NotBlank
     private VoteType voteType;
 
     /**
-     *
+     * The {@link Date} in which this Vote is being cast.
      */
     @Column(nullable = false, updatable = true)
     @Temporal(TemporalType.TIMESTAMP)
@@ -47,23 +55,24 @@ public class Vote implements Domain {
     private Date voteCastDate;
 
     /**
-     *
+     * The {@link User} who is casting this Vote.
      */
     @ManyToOne
     @JoinColumn(name = "cast_by_user_username", nullable = false)
     private User voteCastBy;
 
     /**
-     *
+     * The {@link Entry} that is being Voted on.
      */
     @ManyToOne
     @JoinColumn(name = "entry_id", nullable = true)
     private Entry entry;
 
     /**
+     * Construct a Vote object.
      *
-     * @param voteCastBy
-     * @param voteType
+     * @param voteCastBy The {@link User} who is casting the Vote.
+     * @param voteType   The {@link VoteType} of this Vote.
      */
     public Vote(final User voteCastBy, final VoteType voteType) {
         this.setVoteCastBy(voteCastBy);
@@ -72,9 +81,9 @@ public class Vote implements Domain {
     }
 
     /**
-     *
+     * Default constructor of Vote.
      */
-    protected Vote(){
+    protected Vote() {
 
     }
 
@@ -89,63 +98,70 @@ public class Vote implements Domain {
     }
 
     /**
+     * Return the ID of this Vote.
      *
-     * @return
+     * @return The ID of this Vote.
      */
     public final Long getId() {
         return id;
     }
 
     /**
+     * Get the {@link Date} that this Vote was cast.
      *
-     * @return
+     * @return The {@link Date} that this Vote was cast.
      */
     public final Date getVoteCastDate() {
         return voteCastDate;
     }
 
     /**
+     * Set the {@link Date} that this Vote was cast.
      *
-     * @param voteCastDate
+     * @param voteCastDate The {@link Date} this Vote was cast.
      */
-    public final void setVoteCastDate(Date voteCastDate) {
+    public final void setVoteCastDate(final Date voteCastDate) {
         this.voteCastDate = voteCastDate;
     }
 
     /**
+     * Get the {@link User} who cast this Vote.
      *
-     * @return
+     * @return The {@link User} who cast this Vote.
      */
     public final User getVoteCastBy() {
         return voteCastBy;
     }
 
     /**
+     * Set the {@link User} who cast the Vote.
      *
-     * @param voteCastBy
+     * @param voteCastBy The {@link User} who cast the Vote.
      */
-    public void setVoteCastBy(final User voteCastBy) {
+    public final void setVoteCastBy(final User voteCastBy) {
         this.voteCastBy = voteCastBy;
     }
 
     /**
+     * Get what type of Vote this is, be it Upvote or Downvote.
      *
-     * @return
+     * @return Upvote or Downvote.
      */
     public final VoteType getVoteType() {
         return voteType;
     }
 
     /**
+     * Set this Votes {@link VoteType}, be it an Upvote or a Downvote.
      *
-     * @param voteType
+     * @param voteType Upvote or Downvote.
      */
-    public void setVoteType(final VoteType voteType) {
+    public final void setVoteType(final VoteType voteType) {
         this.voteType = voteType;
     }
 
     @Override
-    public final boolean equals(Object o) {
+    public final boolean equals(final Object o) {
         if (this == o) {
             return true;
         }
