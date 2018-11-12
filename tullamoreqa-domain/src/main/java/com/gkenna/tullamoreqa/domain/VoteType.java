eca1472e1b148ccdddd23fc843eb5e611a680e73
Gavin <thegavinkenna@gmail.com>
2018-11-12 20:44:22 +0000

    #24 Finalised VoteType JavaDocs requirements.

--- tullamoreqa-domain/src/main/java/com/gkenna/tullamoreqa/domain/VoteType.java
+++ tullamoreqa-domain/src/main/java/com/gkenna/tullamoreqa/domain/VoteType.java
@@ -4,22 +4,63 @@
 
 package com.gkenna.tullamoreqa.domain;
 
+/**
+ * VoteType Enum. Specifies the type of Vote, be it Upvote or Downvote
+ * and the {@link Vote}s numerical weight.
+ *
+ * @author Gavin Kenna
+ * @since 0.0.11
+ */
 public enum VoteType {
-    UPVOTE("Upvote", 1), DOWNVOTE("Downvote", -1);
 
+    /**
+     * An Upvote. This carries +1 to the total score of the {@link Entry}.
+     */
+    UPVOTE("Upvote", 1),
+
+    /**
+     * An Upvote. This carries -1 to the total score of the {@link Entry}.
+     */
+    DOWNVOTE("Downvote", -1);
+
+    /**
+     * Textual representation of the Vote Type.
+     * By default there will be two - Upvote and Downvote.
+     */
     private final String voteType;
+
+    /**
+     * Numerical value of the Vote. Upvote will add +1 to the Score,
+     * while a Downvote will add -1 to the score.
+     */
     private final int voteValue;
 
-    VoteType(String voteType, int voteValue) {
+    /**
+     * Default constructor for VoteType enum.
+     *
+     * @param voteType  String representation of the Vote Type.
+     * @param voteValue Numerical value of the Vote.
+     */
+    VoteType(final String voteType, final int voteValue) {
         this.voteType = voteType;
         this.voteValue = voteValue;
     }
 
-    public String getVoteType() {
+    /**
+     * The String representation of the {@link Vote}.
+     *
+     * @return UPVOTE or DOWNVOTE.
+     */
+    public final String getVoteType() {
         return voteType;
     }
 
-    public int getVoteValue() {
+    /**
+     * Return the numerical value of the {@link Vote}.
+     *
+     * @return 1 for Upvote and -1 for Downvote.
+     */
+    public final int getVoteValue() {
         return voteValue;
     }
 }
